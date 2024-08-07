989755bdc14d07a81a72d338c2877602f5e3dfee
Tsoj Tsoj <tsoj.tsoj@gmx.de>
2024-08-07 13:50:23 +0200

    Updated cppfront version

--- setup_cppfront.sh
+++ setup_cppfront.sh
@@ -4,10 +4,15 @@ set -e
 
 mkdir -p ./ext/cppfront/target/
 cd ./ext/cppfront
-git clone --recursive https://github.com/modern-cmake/cppfront
-cd cppfront
-git checkout 581f1aa
-cd ..
+
+if [ ! -d "cppfront" ]; then
+    git clone --recursive https://github.com/tsoj/modern-cmake-cppfront
+    cd cppfront
+    git checkout 7469a30a930db54ea8ecd7c5d585f03ba2c0fd7f
+    cd ..
+fi
+
 cmake -S cppfront -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./target
+cmake --build build --target clean
 cmake --build build --target install
 
